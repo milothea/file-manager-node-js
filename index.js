cc9a2c235ffb9cc520e9aa7c05d0e4944d82ea39
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2023-06-24 14:05:37 +0200

    feat: start implementation of runFileManager method

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const greetUser = () => {
-    const { argv } = process;
-    const usernamePrefix = '--username=';
+import { stdin, stdout } from 'node:process';
+import { COMMANDS, USERNAME_PREFIX } from './config/constants.js';
+
+const getUsername = (args = []) => {
     let username;
 
-    for (let i = 0; i < process.argv.length; i++) {
-        if (argv[i].includes(usernamePrefix)) {
-            username = argv[i].replace(usernamePrefix, '');
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].includes(USERNAME_PREFIX)) {
+            username = args[i].replace(USERNAME_PREFIX, '');
 
             break;
         }
     }
 
-    console.log(`Welcome to the File Manager, ${username || 'Noname'}!`);
+    return username || 'Noname';
 };
 
-greetUser();
+const runFileManager = () => {
+    const { argv } = process;
+    const username = getUsername(argv);
+
+    stdout.write(`Welcome to the File Manager, ${username}!`);
+
+    stdin.pipe((request) => {
+        console.log(request)
+        if (request === COMMANDS.EXIT) {
+            stdout.write(`Thank you for using File Manager, ${username}, goodbye!`);
+            process.exit();
+        }
+    })
+}
+
+runFileManager();
