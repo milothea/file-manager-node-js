14921781bef7f515e248e38a61dc569031232910
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2023-06-25 16:42:09 +0200

    feat: add app constants

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,33 @@
-export const USERNAME_PREFIX = '--username=';
+const DEFAULT_ERROR_MESSAGE = 'Operation failed.';
+const OS_OPERATIONS_COMMAND = 'os';
+const USERNAME_PREFIX = '--username=';
+const DEFAULT_SYSTEM_MESSAGE = '\nEnter your command: ';
 
-export const COMMANDS = {
+const COMMANDS = {
+    ADD: 'add',
+    CAT: 'cat',
+    COMPRESS: 'compress',
+    COPY: 'cp',
+    CPU: 'os --cpus',
+    CPU_ARCH: 'os --architecture',
+    DECOMPRESS: 'decompress',
+    DELETE: 'rm',
+    EOL: 'os --EOL',
     EXIT: '.exit',
+    HASH: 'hash',
+    HOMEDIR: 'os --homedir',
+    GO_TO: 'cd',
+    LIST: 'ls',
+    MOVE: 'mv',
+    RENAME: 'rn',
+    USERNAME: 'os --username',
+    GO_TO_UP: 'up',
+};
+
+export {
+    DEFAULT_ERROR_MESSAGE,
+    DEFAULT_SYSTEM_MESSAGE,
+    OS_OPERATIONS_COMMAND,
+    USERNAME_PREFIX,
+    COMMANDS,
 };
