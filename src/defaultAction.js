7bb37c7a86f892726e975887c6e80efa7d72771e
Liubou Kavaleuskaya <sauchanka.liubou@gmail.com>
2023-06-25 19:17:28 +0200

    refactor: edit message for unimplemented commands handler

--- src/defaultAction.js
+++ src/defaultAction.js
@@ -1,7 +1,8 @@
 import { stdout } from 'node:process';
+import { DEFAULT_SYSTEM_MESSAGE } from '../config/constants.js';
 
 const handleUnimplementedCommands = (command) => {
-    stdout.write(`Unfortunately, I have not implemented the command '${command}'.`);
+    stdout.write(`Unfortunately, I have not implemented the command '${command}'.${DEFAULT_SYSTEM_MESSAGE}`);
 };
 
 const quitProcess = (username) => {
